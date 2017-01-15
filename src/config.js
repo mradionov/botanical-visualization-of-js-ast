e5d70497024147c84a26571e481de3e9a98b1473
Michael Radionov <micrddrgn@gmail.com>
2017-01-15 23:24:14 +0300

    Increase tilt

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 (function () {
 
+  const scale = 40;
+  const ratio = 2.5;
+
   const config = {
 
-    STEM_WIDTH: 5,
-    STEM_HEIGHT: 20,
+    STEM_WIDTH: scale,
+    STEM_HEIGHT: scale * ratio,
 
-    TILT: 60,
+    TILT: 90,
 
   };
 
